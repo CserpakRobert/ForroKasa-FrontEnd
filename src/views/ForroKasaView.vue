ef773d20f94f3e9161da33cb6fd2b344dabe3c24
CserpakRobert <robi.cserpak@gmail.com>
2022-03-09 14:19:31 +0100

    axios nem megy :(

--- src/views/ForroKasaView.vue
+++ src/views/ForroKasaView.vue
@@ -1,18 +1,27 @@
 <script setup lang="ts">
-import axios from "axios"
-import { log } from "console"
+  import axios from "axios";
+  import { log } from "console";
 
-let searchParams: any = {
+  let searchParams: any = {
     start: 0,
     amount: 10,
     sort: 0,
-    category: '',
-    userId: '',
-    filter: ''
-};
-let recipes = ref(await axios.get('localhost:5000/recipes').then(req => console.log(req), err => console.log(err)));
-console.log(recipes.value)
+    category: "",
+    userId: "",
+    filter: "",
+  };
+  let recipes: any[] | any = [];
+  axios.get("http://localhost:5000/recipes").then(
+    (req) => (recipes = ref(req)),
+    (err) => console.log(err)
+  );
+  console.log(recipes.value);
 </script>
+
 <template>
-    <h1>Hello World</h1>
+  <h1>Hello World</h1>
+  <table>
+    <td>asdsad</td>
+    <td v-for="recipe in this.recipes">{{ "asd" + recipe }}</td>
+  </table>
 </template>
