dd007171b5d8a4434cdb0ca54c5e4516b5b4ee0d
CserpakRobert <robi.cserpak@gmail.com>
2022-03-16 14:21:54 +0100

    :(

--- src/views/ForroKasaView.vue
+++ src/views/ForroKasaView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
   import axios from "axios";
   import { log } from "console";
+  import { Ref } from "vue";
 
   let searchParams: any = {
     start: 0,
@@ -10,18 +11,27 @@
     userId: "",
     filter: "",
   };
-  let recipes: any[] | any = [];
+  let recipes: any | Ref = ref(["léáéláélá", ",.-.,-,.-"]);
   axios.get("http://localhost:5000/recipes").then(
-    (req) => (recipes = ref(req)),
+    (req) => {
+      recipes = ref(req.data);
+      console.log(req.data);
+      console.log(recipes);
+      console.log(recipes.value);
+      for (let item of recipes) {
+        console.log(item._id);
+      }
+    },
     (err) => console.log(err)
   );
-  console.log(recipes.value);
 </script>
 
 <template>
   <h1>Hello World</h1>
   <table>
     <td>asdsad</td>
-    <td v-for="recipe in this.recipes">{{ "asd" + recipe }}</td>
+    <p v-for="recipe of this.recipes">
+      {{ "asd" + recipe }}
+    </p>
   </table>
 </template>
